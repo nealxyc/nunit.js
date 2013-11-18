d0a55a4f019099dbc05e9cb71b2578018dab408a
nxiong <neal.xiong@gtnexus.com>
2013-11-18 14:13:43 -0800

    Changed test server to run on connect.

--- startTestServer.bat
+++ startTestServer.bat
@@ -1,3 +1,2 @@
 start chrome "http://localhost:8001/test/"
-python -m SimpleHTTPServer 8001  > http.log 2> http.err  & 
-
+node server.js  > http.log 2> http.err &
--- startTestServer.sh
+++ startTestServer.sh
@@ -4,5 +4,5 @@ ROOT=$(pwd)
 echo 'Root folder: ' $ROOT
 cd $ROOT
 open "http://localhost:8001/test/"
-python -m SimpleHTTPServer 8001  > http.log 2> http.err  & 
+node server.js  > http.log 2> http.err &
 
